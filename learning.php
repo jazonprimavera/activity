65a1a43aca34a4905afd2347a87d80c54dfd9a5d
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-09-07 09:20:51 +0800

    Update learning.php

--- learning.php
+++ learning.php
@@ -1,25 +1,27 @@
-<!DOCTYPE html>
- <html>
- <head>
- <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,600;0,900;1,200;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">
- <link rel="stylesheet" type="text/css" href="style.css">
- </head>
-
- <body>
- 
- <div class="header">
-    <div class="inner_header">
-	<div class="logo_container">
-	    <h1>MY<span>SITE</span></h1>
-       </div>
-	   
-	   <ul class="navigation">
-	   <a><li>Home</li></a>
-	   <a><li>About</li></a>
-	   <a><li>Portfolio</li></a>
-	   <a><li>Contact</li></a>
-	   </ul>
-	  </div>
-	 </div>
-   </body>
- </html>
+<? php
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,600;0,900;1,200;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">';
+echo '<link rel="stylesheet" type="text/css" href="style.css">';
+echo '</head>';
+echo '';
+echo '<body>';
+echo '';
+echo '<div class="header">';
+echo '<div class="inner_header">';
+echo '<div class="logo_container">';
+echo '<h1>MY<span>SITE</span></h1>';
+echo '</div>';
+echo '';
+echo '<ul class="navigation">';
+echo '<a><li>Home</li></a>';
+echo '<a><li>About</li></a>';
+echo '<a><li>Portfolio</li></a>';
+echo '<a><li>Contact</li></a>';
+echo '</ul>';
+echo '</div>';
+echo '</div>';
+echo '</body>';
+echo '</html>';
+?>
